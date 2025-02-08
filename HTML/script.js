2020fe4dcd4ebd4325b04b5eb6b115f3909d08aa
Manuel <marcdaniel.manuel@sdca.edu.ph>
2025-02-08 11:33:05 +0800

    too sigma to care nonchalant king

--- HTML/script.js
+++ HTML/script.js
@@ -3,12 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
-        })
+
+            const toggleButton = document.getElementById('theme-toggle');
+
+            const currentMode = localStorage.getItem('theme') || 'light';
+            if (currentMode === 'dark') {
+                document.body.classList.add('dark-mode');
+            }
+
+            toggleButton.addEventListener('click', () => {
+                document.body.classList.toggle('dark-mode');
+
+                if (document.body.classList.contains('dark-mode')) {
+                    localStorage.setItem('theme', 'dark');
+                } else {
+                    localStorage.setItem('theme', 'light');
+                }
+            });
+        });
 
     fetch("footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
-        })
-
+        });
 });
